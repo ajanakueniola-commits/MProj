1c1b16cedcb68c86ed163fed8f703dc28ec87184
Funmi123 <ajanakueniola@gmail.com>
2026-01-15 15:56:01 +0100

    updated variable.tf files code

--- terraform/variable.tf
+++ terraform/variable.tf
@@ -24,14 +24,17 @@ variable "packer_ami_name_pattern" {
 
 variable "db_username" {
   type = string
+  default = "appuser"
 }
 
 variable "db_password" {
   type      = string
   sensitive = true
+  default = "StrongPassw0rd!2025"
 }
 variable "db_name" {
   type = string
+  default = "gracedb"
 }
 variable "private_subnet_cidrs" {
   type = list(string)
@@ -46,6 +49,6 @@ variable "azs" {
   default     = ["us-east-2a", "us-east-2b"]
 }
 variable "key_name" {
-  description = "AWS EC2 key pair name"
   type        = string
+  default     = "ohio123"
 }
